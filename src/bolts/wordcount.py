f7c0a28f51a1194581f1625caf0b00c4b9011c78
Munaf A Qazi <munaf.qazi@gmail.com>
2016-10-15 16:50:31 -0400

    Json to mongo

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -1,4 +1,6 @@
 import os
+import json
+from pymongo import MongoClient
 from collections import Counter
 
 from streamparse import Bolt
@@ -12,14 +14,24 @@ class WordCountBolt(Bolt):
         self.pid = os.getpid()
         self.total = 0
 
-    def _increment(self, word, inc_by):
-        self.counter[word] += inc_by
-        self.total += inc_by
-
     def process(self, tup):
-        word = tup.values[0]
-        self._increment(word, 10 if word == "dog" else 1)
-        if self.total % 1000 == 0:
-            self.logger.info("counted [{:,}] words [pid={}]".format(self.total,
-                                                                    self.pid))
-        self.emit([word, self.counter[word]])
+        jsonFiles = tup.values[0];
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['weisheng']
+
+        counter = 0
+        #    for jsonFile in jsonFiles:
+        with open(jsonFiles, 'r') as f:
+            for line in f:
+                # load valid lines (should probably use rstrip)
+                if len(line) < 10: continue
+                db['yelpDataMain'].insert(json.load(line))
+                counter += 1
+                print "loaded line: ", counter
+        f.close
+        db.close
+
+        if 0 == counter:
+            print "Warning: No Records were Loaded"
+        else:
+            print "loaded a total of ", counter, " lines"
