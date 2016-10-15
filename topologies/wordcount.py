8e767a7e2491dd6e0524e9867dc66aa70ca7daf0
muraleo <muraleo80923@gmail.com>
2016-10-15 19:18:51 -0400

    Revert "Json to mongo"

--- topologies/wordcount.py
+++ topologies/wordcount.py
@@ -4,11 +4,15 @@ Word count topology
 
 from streamparse import Grouping, Topology
 
-from bolts.yelpdatamain import YelpDataMainBolt
+from bolts.wordcount import WordCountBolt
+from spouts.words import WordSpout
 from spouts.yelp import YelpSpout
 
 
 class WordCount(Topology):
+    word_spout = WordSpout.spec()
     yelp_spout = YelpSpout.spec()
-    new_count_bolt = YelpDataMainBolt.spec(inputs={yelp_spout: Grouping.fields('searchJson')},
+    count_bolt = WordCountBolt.spec(inputs={word_spout: Grouping.fields('word')},
+                                    par=2)
+    new_count_bolt = WordCountBolt.spec(inputs={yelp_spout: Grouping.fields('searchJson')},
                                         par=2)
