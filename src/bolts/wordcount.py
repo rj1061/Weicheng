52ef07e8862693ca78e3720f35971145efa53f7a
Munaf A Qazi <munaf.qazi@gmail.com>
2016-10-15 19:08:41 -0400

    Fully working saving model

--- src/bolts/wordcount.py
+++ src/bolts/wordcount.py
@@ -1,37 +1,21 @@
-import os
 import json
 from pymongo import MongoClient
-from collections import Counter
 
 from streamparse import Bolt
 
 
 class WordCountBolt(Bolt):
-    outputs = ['word', 'count']
-
-    def initialize(self, conf, ctx):
-        self.counter = Counter()
-        self.pid = os.getpid()
-        self.total = 0
 
     def process(self, tup):
         jsonFiles = tup.values[0];
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client['weisheng']
-
-        counter = 0
-        #    for jsonFile in jsonFiles:
-        with open(jsonFiles, 'r') as f:
-            for line in f:
-                # load valid lines (should probably use rstrip)
-                if len(line) < 10: continue
-                db['yelpDataMain'].insert(json.load(line))
-                counter += 1
-                print "loaded line: ", counter
-        f.close
-        db.close
-
-        if 0 == counter:
-            print "Warning: No Records were Loaded"
+        try:
+            dictData = json.loads(jsonFiles)
+        except ValueError:
+            self.logger.info('Empty JSON or bad Response ' + jsonFiles)
         else:
-            print "loaded a total of ", counter, " lines"
+            client = MongoClient('mongodb://localhost:27017/')
+            db = client['weisheng']
+
+            for business in dictData['businesses']:
+                db['yelpDataMain'].insert(business)
+            db.close
--- topologies/wordcount.py
+++ topologies/wordcount.py
@@ -5,14 +5,10 @@ Word count topology
 from streamparse import Grouping, Topology
 
 from bolts.wordcount import WordCountBolt
-from spouts.words import WordSpout
 from spouts.yelp import YelpSpout
 
 
 class WordCount(Topology):
-    word_spout = WordSpout.spec()
     yelp_spout = YelpSpout.spec()
-    count_bolt = WordCountBolt.spec(inputs={word_spout: Grouping.fields('word')},
-                                    par=2)
     new_count_bolt = WordCountBolt.spec(inputs={yelp_spout: Grouping.fields('searchJson')},
                                         par=2)
