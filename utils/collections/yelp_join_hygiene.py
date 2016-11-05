bbfbc6b18314cf8200bd69a1b120dd0eeb39bab5
Leo <psk285@nyu.edu>
2016-11-05 16:46:28 -0400

    Corrected some stuff Munaf told

--- utils/collections/yelp_join_hygiene.py
+++ utils/collections/yelp_join_hygiene.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.weisheng
+db = client['weisheng']
 test = {"test":"test"}
-collection = db.yelp_join_hygiene.insert(test)
-collection = db.yelp_join_hygiene.remove({})
+db.yelp_join_hygiene.insert(test)
+db.yelp_join_hygiene.remove({})
