389847adb292e482738c947dd1ee97abdb2d0267
Leo <psk285@nyu.edu>
2016-11-05 18:25:28 -0400

    Hygiene dataset split into two

---
+++ utils/import_data.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mongoimport --db weisheng --collection hygiene --type csv --headerline --file ../data/DOHMH_New_York_City_Restaurant_Inspection_Results_1.csv
+mongoimport --db weisheng --collection hygiene --type csv --headerline --file ../data/DOHMH_New_York_City_Restaurant_Inspection_Results_2.csv
