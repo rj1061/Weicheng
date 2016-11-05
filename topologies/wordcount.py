d945a423bcb93e496d1de5e79ae8a5e999c9a4f4
Munaf A Qazi <munaf.qazi@gmail.com>
2016-11-05 17:37:32 -0400

    * new spout for retrieving data from hygiene collection in mongo

--- topologies/wordcount.py
+++ topologies/wordcount.py
@@ -4,11 +4,7 @@ Word count topology
 
 from streamparse import Grouping, Topology
 
-from bolts.yelpdatamain import YelpDataMainBolt
-from spouts.yelp import YelpSpout
-
+from spouts.readHygiene import readHygiene
 
 class WordCount(Topology):
-    yelp_spout = YelpSpout.spec()
-    new_count_bolt = YelpDataMainBolt.spec(inputs={yelp_spout: Grouping.fields('searchJson')},
-                                        par=2)
+    yelp_spout = readHygiene.spec()
